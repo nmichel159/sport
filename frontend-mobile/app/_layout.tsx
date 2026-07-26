2a5b61fd46b5b5ac148ae70ad3225a8ae9fdbcea
Norbert Michel <noro.michel159gmail.com>
2026-07-26 16:38:15 +0200

    zakladne obrazovky

--- frontend-mobile/app/_layout.tsx
+++ frontend-mobile/app/_layout.tsx
@@ -2,4 +2,4 @@ import { Stack } from 'expo-router'
 import { AuthProvider } from '../auth'
 import { I18nProvider } from '../i18n'
 
-export default function RootLayout() { return <I18nProvider><AuthProvider><Stack><Stack.Screen name="index" options={{ title: 'Sport' }} /><Stack.Screen name="oauth2redirect" options={{ headerShown: false }} /></Stack></AuthProvider></I18nProvider> }
+export default function RootLayout() { return <I18nProvider><AuthProvider><Stack><Stack.Screen name="index" options={{ headerShown: false }} /><Stack.Screen name="oauth2redirect" options={{ headerShown: false }} /></Stack></AuthProvider></I18nProvider> }
